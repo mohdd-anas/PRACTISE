2eeba2255c61b383a45b87eb88d4d46ec138ee90
MohdAnas <mohdanas48925@gmail.com>
2025-06-20 23:29:39 +0530

    ARRAY QUESTION ADDED

---
+++ ARRAY/QUESTION/swap.java
@@ -0,0 +1,20 @@
+package ARRAY.QUESTION;
+import java.util.*;
+public class swap {
+    public static void main(String[] args)
+    {
+        int[] arr={1,2,3,4,5};
+        System.out.println(Arrays.toString(arr)); //In java primitives value is copied and object are by reference 
+        swap(arr,1,4);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap(int [] arr,int index1,int index2)
+    {
+        int temp=arr[index1];
+        arr[index1]=arr[index2];
+        arr[index2]=temp;
+    }
+
+}
